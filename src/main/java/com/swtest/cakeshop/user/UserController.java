03f56288b7c6925643a122ca2456210390482ab1
Tuấn Anh <phantuananh0202@gmail.com>
2024-11-16 18:26:51 +0700

    feat: update documentation and add methods to handle user updates and delete

--- src/main/java/com/swtest/cakeshop/user/UserController.java
+++ src/main/java/com/swtest/cakeshop/user/UserController.java
@@ -1,7 +1,9 @@
 package com.swtest.cakeshop.user;
 
-import com.swtest.cakeshop.person.PersonService;
-import com.swtest.cakeshop.person.dto.PersonResponse;
+import com.swtest.cakeshop.user.person.Person;
+import com.swtest.cakeshop.user.person.PersonService;
+import com.swtest.cakeshop.user.person.dto.PersonRequest;
+import com.swtest.cakeshop.user.person.dto.PersonResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -9,23 +11,22 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     private final PersonService personService;
     private final UserService userService;
+    private final UserRepository userRepository;
 
-    public UserController(PersonService personService, UserService userService) {
+    public UserController(PersonService personService, UserService userService, UserRepository userRepository) {
         this.personService = personService;
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
-    @Operation(summary = "Get user information", tags = {"User"})
+    @Operation(summary = "Get current user information", tags = {"User"})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "User information found", content = @Content(schema = @Schema(implementation = PersonResponse.class))),
             @ApiResponse(responseCode = "404", description = "User information not found", content = @Content)
@@ -45,5 +46,28 @@ public class UserController {
     public ResponseEntity<PersonResponse> getUserInfo(@PathVariable String username) {
         User user = userService.findByUsername(username);
         return ResponseEntity.ok(personService.findPersonByUser(user).toDTO());
+    }
+
+    @Operation(summary = "Update current user information", tags = {"User"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User information changed successfully", content = @Content),
+            @ApiResponse(responseCode = "404", description = "User information not found", content = @Content)
+    })
+    @PutMapping
+    public ResponseEntity<Void> updateUserInfo(@RequestBody PersonRequest personRequest) {
+        User currentUser = userService.getCurrentUser();
+        Person updatedPerson = personService.updatePerson(personRequest, currentUser);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "Delete current user with information", tags = {"User"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Delete user successfully", content = @Content)
+    })
+    @DeleteMapping
+    public ResponseEntity<Void> deleteUserInfo() {
+        User currentUser = userService.getCurrentUser();
+        userRepository.delete(currentUser);
+        return ResponseEntity.noContent().build();
     }
 }
